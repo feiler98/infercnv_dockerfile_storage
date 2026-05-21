7d54428e14cd6c1b0464ff9af918163716dd2ad8
feiler98 <werner.feiler@ukmuenster.de>
2026-05-21 15:25:06 +0200

    new files

---
+++ CopyKat/script.R
@@ -0,0 +1,3 @@
+install.packages(c('plotly', 'parallelDist', 'dlm', 'gplots', 'RColorBrewer', 'mixtools', 'MCMCpack'), dependencies=TRUE, repos = 'http://cran.rstudio.com/')
+install.packages('./copykat/', repos=NULL, type='source')
+library(copykat)
